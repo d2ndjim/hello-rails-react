12553fce40d9a4c863ad8d169b42453c5c8dd82f
Lekan <jimoridwanlekan@gmail.com>
2022-09-12 17:03:39 +0100

    Modify controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   def index
     @message = Message.order(Arel.sql('RANDOM()')).first
-    render json: { message: @message.greeting }
+    /api/v1/messages/api/v1/messages
   end
 end
